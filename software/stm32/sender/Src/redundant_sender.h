e37ffd0c81ae8459b7770187f700c1875d0cbbac
LeeMinJun-git <leemj2616@gmail.com>
2026-08-10 10:23:23 +0900

    Add files via upload

---
+++ software/stm32/sender/Src/redundant_sender.h
@@ -0,0 +1,15 @@
+#ifndef REDUNDANT_SENDER_H
+#define REDUNDANT_SENDER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void RedundantSender_Init(void);
+void RedundantSender_Process(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* REDUNDANT_SENDER_H */
